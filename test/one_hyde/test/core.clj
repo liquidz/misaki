90a647575824783b708ff12ff9e14e93139e3bee
liquidz <liquidz.uo@gmail.com>
2012-03-21 18:28:58 +0900

    add tests for one-hyde.transform

--- test/one_hyde/test/core.clj
+++ test/one_hyde/test/core.clj
@@ -1,8 +1,19 @@
 (ns one-hyde.test.core
-  (:use one-hyde.core)
+  (:use one-hyde.core
+        one-hyde.transform)
   (:use [clojure.test]))
 
 (deftest replace-me ;; FIXME: write
   (is true))
 
 
+;;; TRANSFORM
+(deftest transform-test
+  (binding [*transformers* (atom [inc #(* 2 %)])]
+    (is (= 4 (transform 1)))
+    (add-transformer! inc)
+    (is (= 12 (transform #(* 3 %)))))
+
+  (let [f (transform "(apply + site)")]
+    ; f => (fn [site & contents] (list (apply + site)))
+    (is (= '(6) (f [1 2 3])))))
