c7ac8a77c17d847c9288384b9bb90b73e2351551
Masashi Iizuka <liquidz.uo@gmail.com>
2012-04-15 23:47:42 +0900

    inc version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             [clj-time "0.3.7"]
             [uochan/watchtower "0.1.2"]])
 
-(defproject misaki "0.0.14-alpha"
+(defproject misaki "0.0.15-alpha"
   :description "Static site generator in Clojure inspired by Jekyll"
   :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
   :dev-dependencies [[lein-multi "1.1.0"]
