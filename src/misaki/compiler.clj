a52fdc04af2cd8059d793f4ae24cae2fb638b9a7
uochan <liquidz.uo@gmail.com>
2012-05-01 00:21:32 +0900

    editing compiler.clj

---
+++ src/misaki/compiler.clj
@@ -0,0 +1,27 @@
+(ns misaki.compiler
+  (:use [misaki config loader evaluater]
+        [hiccup.core :only [html]])
+  (:require [clojure.string :as str])
+  )
+
+(defn make-site-data []
+  (assoc *site*
+         :posts ()
+         :tags ()
+         :date ""))
+
+;(defn get-compile-fn []
+;  )
+
+;(defn compile-template
+;  [#^File file]
+;  (let [tmpl-fn  (-> file load-template evaluate-template)
+;        filename (make-template-output-filename file)
+;        data (with-meta '() (meke-site-data))
+;        result ( (apply-template tmpl-fn data))
+;        ]
+;
+;
+;
+;    )
+;  )
