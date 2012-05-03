af40d58f57a7cb010b1e1dd7574c2859a7ae08ec
uochan <liquidz.uo@gmail.com>
2012-05-03 11:37:32 +0900

    not to use lein-multi

--- project.clj
+++ project.clj
@@ -10,22 +10,22 @@
                  [uochan/watchtower "0.1.2"]
                  [de.ubercode.clostache/clostache "1.3.0"]]
 
-  :dev-dependencies [[lein-multi "1.1.0"]
+  :dev-dependencies [;[lein-multi "1.1.0"]
                      [lein-marginalia "0.7.0"]
                      [debug-reader "0.0.1"]]
 
   :repositories [["sonatype-snapshots"
                   "https://oss.sonatype.org/content/repositories/snapshots/"]]
 
-  :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]]
-               "1.3"   [[org.clojure/clojure "1.3.0"]]
-               "1.5S"  [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
-               :all [[org.clojure/core.incubator "0.1.0"]
-                     [org.clojure/clojurescript "0.0-1011"]
-                     [hiccup "0.3.6"]
-                     [compojure "1.0.2"]
-                     [ring/ring-jetty-adapter "1.0.2"]
-                     [clj-time "0.3.7"]
-                     [uochan/watchtower "0.1.2"]]}
+  ;:multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]]
+  ;             "1.3"   [[org.clojure/clojure "1.3.0"]]
+  ;             "1.5S"  [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
+  ;             :all [[org.clojure/core.incubator "0.1.0"]
+  ;                   [org.clojure/clojurescript "0.0-1011"]
+  ;                   [hiccup "0.3.6"]
+  ;                   [compojure "1.0.2"]
+  ;                   [ring/ring-jetty-adapter "1.0.2"]
+  ;                   [clj-time "0.3.7"]
+  ;                   [uochan/watchtower "0.1.2"]]}
 
   :main misaki.server)
