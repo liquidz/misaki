7983b140c413b22463af4d2598f8686b290476c6
Masashi Iizuka <liquidz.uo@gmail.com>
2012-06-12 00:18:12 +0900

    delete map-value

--- src/misaki/util/sequence.clj
+++ src/misaki/util/sequence.clj
@@ -14,7 +14,3 @@
   ([ls]   (sort-alphabetically identity ls))
   ([f ls] (sort #(neg? (.compareTo (f %) (f %2))) ls)))
 
-
-(defn map-value [f m]
-  (into {} (for [[k v] m]
-             [k (if (map? v) (map-value f v) (f v))])))
