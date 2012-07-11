8e50553181e85e92b7b5fdb4dead2133613cb462
uochan <liquidz.uo@gmail.com>
2012-07-11 09:08:59 +0900

    update document

--- doc/.template/index.html.clj
+++ doc/.template/index.html.clj
@@ -5,13 +5,15 @@
   (:title site)
   (p "misaki Documents"))
 
+(h2 "misaki?")
+(p "misaki is [Jekyll](https://github.com/mojombo/jekyll) inspired static site generator in [Clojure](http://clojure.org).")
+
 (h2 "Table of Contents")
 (ul
   #(link (:title %) (:url %))
   (:posts site))
 
 (h2 "API")
-
 (ul
   [(link "API Index" "api/uberdoc.html")
    (list "Built in functions"
@@ -24,3 +26,8 @@
 (h2 "Demo")
 (ul [(link "http://liquidz.github.com/misaki/")])
 
+
+(h2 "License")
+(p "Copyright (C) 2012 Masashi Iizuka([@uochan](http://twitter.com/uochan/))
+
+   Distributed under the Eclipse Public License, the same as Clojure.")
