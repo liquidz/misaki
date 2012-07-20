cfc2124cae015dd5a0cff872ce57aa98733df61a
liquidz <liquidz.uo@gmail.com>
2012-07-21 08:08:40 +0900

    #14 update misaki.util.error

--- src/misaki/util/error.clj
+++ src/misaki/util/error.clj
@@ -26,43 +26,45 @@
 
 (defn- get-stack-trace [#^Throwable ex]
   (let [traces (seq (.getStackTrace ex))]
-    (map stack-trace->map traces)
-    )
-  )
+    (map stack-trace->map traces)))
 
 (defn throwable->map [ex]
-  {:message (.getMessage ex)
-   :stack-trace (get-stack-trace ex)
-   :str     (.toString ex)
-   :localized-message (.getLocalizedMessage ex)
-   :causes  (get-causes ex)
-   })
+  {:message           (.getMessage ex)
+   :stack-trace       (get-stack-trace ex)
+   :causes            (get-causes ex)
+   :str               (.toString ex)
+   :localized-message (.getLocalizedMessage ex)})
 
 
-(defn map-color-format [m & pair]
-  (merge m (into {} (for [[key color-fn] (partition 2 pair)]
+; =assoc-color
+(defn assoc-color
+  "Associate color-function to hash-map
+
+  ex)
+    (assoc-color {:a \"AAA\", :b \"BBB\"}
+                  :a red
+                  :b blue)"
+  [m & kvs]
+  (merge m (into {} (for [[key color-fn] (partition 2 kvs)]
                       [key (color-fn (get m key))]))))
 
+(defn- str-contains? [s target]
+  (not= -1 (.indexOf s target)))
 
 (defn- print-cause [cause & {:keys [caused?] :or {caused? false}}]
-  (let [label (str (if caused? "Caused by " "") (:str cause))]
+  (let [label  (str (if caused? "Caused by " "") (:str cause))
+        traces (filter #(str-contains? (:str %) "misaki") (:stack-trace cause))]
     (flush)
     (println "")
-    (println label)
-    (println (apply str (repeat (count label) "~")))
-    (doseq [st (filter #(not= -1 (.indexOf (:str %) "misaki")) (:stack-trace cause))]
-      (println
-        "  at"
-        (underline
-          (str (:class st) "/"
-               (bold (:method st))))
-        (red (str "(" (:filename st) ":" (bold (:line st)) ")")))
-      )))
+    (println (underline label))
+    (doseq [st traces]
+      (let [st (assoc-color st :method bold :filename red :line (comp red bold))]
+        (println
+          (render "   at {{class}} / {{method}} ({{filename}}:{{line}})" st))))))
 
 (defn print-pretty-stack-trace
   [#^Exception ex]
   (let [[f & r] (:causes (throwable->map ex))]
     (print-cause f)
-    (doseq [x r] (print-cause x :caused? true))
-    ))
+    (doseq [x r] (print-cause x :caused? true))))
 
