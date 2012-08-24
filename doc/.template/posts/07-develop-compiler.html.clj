70029557a9851679077486deefe1f8a11c28f16e
liquidz <liquidz.uo@gmail.com>
2012-08-25 08:57:57 +0900

    update developing compiler document

--- doc/.template/posts/07-develop-compiler.html.clj
+++ doc/.template/posts/07-develop-compiler.html.clj
@@ -73,7 +73,89 @@ SH
   (str (:text config) (slurp file)))
 CLJ
 
-(p "You can use [misaki-clostache](https://github.com/liquidz/misaki-clostache) and [misaki API](../api/uberdoc.html) as reference of developing misaki compiler.")
+(h3 "-extension")
+(p "Return file extension list to watch template file.
+   This function is **REQUIRED**.")
 
+(h3 "-config")
+(p "Basic config data is passed, and return customized config data.
+   This function is **OPTIONAL**.")
+
+(h3 "-compile")
+(p "Updated file(java.io.File) is passed, and return compile result.
+   This function is **REQUIRED**.")
+(p "Return value specification is following.")
+
+(dl
+  {:string (list (p "Write string to file which is named as same as template file.")
+##CLJ
+(defn -compile [file]
+  (str "hello " (slurp file)))
+; foo.txt whose body is "world" is passed,
+; result file is *public-dir*/foo.txt whose body is "hello world"
+CLJ)
+   :boolean (list (p "misaki.core does not write file, only check compile result.")
+##CLJ
+(defn -compile [file]
+  (compile-in-compiler file)
+  true)
+; true  => compile success
+; false => compile failed
+CLJ)
+   :symbol (list (p "misaki.core skip the file.")
+##CLJ
+(defn -compile [file]
+  (if-not (valid-file? file) 'skip))
+; Skip to compile file
+CLJ)
+   :map (list (p "Write file with detailed setting.")
+##CLJ
+(defn -compile [file]
+  {; compile result
+   :status        true
+   ; output filename
+   :filename      (str (.getName file) ".html")
+   ; output body
+   :body          (str "hello " (slurp file))
+   ; flag to stop compilation
+   :stop-compile? false
+   ; flat to force compiling all templates
+   :all-compile?  false})
+CLJ)})
+
+(h2 "Test your compiler")
+(p "misaki provides utilities to test your own compiler.")
+
+##CLJ
+(ns foo.test
+  (:use misaki.tester))
+
+; set base directory which include _config.clj
+; default testing base directory is "test"
+(set-base-dir! "test")
+
+; get compiler's config which is customized by `-config`
+(get-config)
+
+; call `-compile` to test your compling
+(let [file (io/file "testtemplate")]
+  (test-compile file))
+
+; define test wrapping config data as *config*
+;
+; This code is same as following:
+;   (deftest foo-test
+;     (binding [*base-dir* "test"
+;               *config* (get-config)]
+;       (is (= "hello") (:template-dir *config*))))
+(deftest* foo-test
+  (is (= "hello") (:template-dir *config*)))
+CLJ
+
+(p "Check [misaki.tester](/misaki/api/uberdoc.html#misaki.tester) to get detailed document.")
+
+(h2 "Another compiler sample")
+
+(p "You can use [misaki-clostache](https://github.com/liquidz/misaki-clostache) and [misaki API](../api/uberdoc.html) as reference of developing misaki compiler.")
 
 (see-also ["Switch Compiler"])
