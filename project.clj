60e7310e7697ddc3b013f2d7ead693384eefc5dd
liquidz <liquidz.uo@gmail.com>
2012-09-28 00:23:59 +0900

    ver up in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject misaki "0.2.2-beta"
+(defproject misaki "0.2.3-beta"
   :description "Jekyll inspired static site generator in Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
